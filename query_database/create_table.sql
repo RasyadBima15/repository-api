3743d4f31f7e38feb44d95f6f40a7b810dae7719
RasyadBima15 <rasyadbimasatya04@gmail.com>
2024-03-20 19:16:42 +0800

    first commit

---
+++ query_database/create_table.sql
@@ -0,0 +1,21 @@
+CREATE TABLE data_dokumen (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nip VARCHAR(10),
+    type_dokumen VARCHAR(50),
+    nama_dokumen VARCHAR(100),
+    nama_file VARCHAR(100),
+    FOREIGN KEY (nip) REFERENCES data_dosen(nip) ON DELETE SET NULL
+);
+
+CREATE TABLE data_dosen (
+    nip VARCHAR(10) PRIMARY KEY,
+    nama_lengkap VARCHAR(100),
+    prodi_id INT,
+    FOREIGN KEY (prodi_id) REFERENCES data_prodi(id) ON DELETE SET NULL
+);
+
+CREATE TABLE data_prodi (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    kode_prodi VARCHAR(10),
+    nama_prodi VARCHAR(100)
+);
